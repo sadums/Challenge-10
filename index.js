c1308f14d800321e1dd10f43a8211706015a1f21
sadums <samjadams4@gmail.com>
2023-05-21 15:40:26 -0600

    added basic prompts

--- index.js
+++ index.js
@@ -2,4 +2,86 @@
 const inquirer = require('inquirer');
 const {createRequest, readRequest, updateRequest, deleteRequest} = require('./server');
 
+const firstQuestions = [
+    {
+        type: 'list',
+        message: 'What would you like to do?',
+        name: 'answer',
+        choices: [
+            'View All Employees',
+            'Add Employee',
+            'Update Employee Role',
+            'View All Roles',
+            'Add Role',
+            'View All Departments',
+            'Add Department',
+            'Quit'
+        ]
+    }
+];
 
+const addEmployeeQuestions = [
+    {
+        
+    }
+]
+
+const updateEmployeeQuestions = [
+    {
+
+    }
+]
+
+const addRoleQuestions = [
+    {
+
+    }
+]
+
+const addDepartmentQuestions = [
+    {
+
+    }
+]
+
+const main = function(){
+    inquirer.prompt(firstQuestions).then((data) => {
+        console.log(data.answer);
+        switch(data.answer){
+            case 'View All Employees':
+                console.log(readRequest("employee"));
+                break;
+            case 'Add Employee':
+                inquirer.prompt(addEmployeeQuestions).then((data) => {
+                    console.log(data)
+                });
+                break;
+            case 'Update Employee Role':
+                inquirer.prompt(addEmployeeQuestions).then((data) => {
+                    console.log(data)
+                });
+                break;
+            case 'View All Roles':
+                console.log(readRequest("role"));
+                break;
+            case 'Add Role':
+                inquirer.prompt(addEmployeeQuestions).then((data) => {
+                    console.log(data)
+                });
+                break;
+            case 'View All Departments':
+                console.log(readRequest("department"));
+                break;
+            case 'Add Department':
+                inquirer.prompt(addEmployeeQuestions).then((data) => {
+                    console.log(data)
+                });
+                break;
+            case 'Quit':
+                return;
+        }
+        main()
+    });
+}
+
+main();
--- server.js
+++ server.js
@@ -15,37 +15,49 @@ const db = mysql.createConnection(
   console.log(`Connected to the courses_db database.`)
 );
 
+
+
+
 // CRUD functions
 const createRequest = () => {
     db.query(`query`, inputs, (err, result) => {
         if (err) {
-        console.log(err);
+            console.log(err);
+            return err;
         }
         console.log(result);
+        return results;
     });
 };
-const readRequest = () => {
-    db.query(`query`, inputs, (err, result) => {
+const readRequest = (input) => {
+    // Not vulnerable to SQL injection as user does not write input
+    db.query(`SELECT * FROM ${input}`, (err, result) => {
         if (err) {
-        console.log(err);
+            console.log(err);
+            return err;
         }
         console.log(result);
+        return result
     });
 };
 const updateRequest = () => {
         db.query(`query`, inputs, (err, result) => {
         if (err) {
-        console.log(err);
+            console.log(err);
+            return err;
         }
         console.log(result);
+        return results
     });
 };
 const deleteRequest = () => {
         db.query(`query`, inputs, (err, result) => {
         if (err) {
-        console.log(err);
+            console.log(err);
+            return err
         }
         console.log(result);
+        return result
     });
 };
 
